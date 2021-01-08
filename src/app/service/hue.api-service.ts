d2d1f29aa3c72768ba12176ce2ccca4f4ed74ae7
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2021-01-08 19:29:55 +0100

    add service

---
+++ src/app/service/hue.api-service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LightStateInterface } from '../data/light-state.interface';
+import { ApiConfig } from './api-config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HueApiService
+{
+
+  private readonly bridgeIP:string = ApiConfig.bridgeIp;
+  private readonly user:string = ApiConfig.user;
+  private readonly url:string = 'https://' + this.bridgeIP + '/api/' + this.user;
+
+  constructor(private http:HttpClient)
+  {
+  }
+
+  public turnOffLight(lightId:string):Observable<any>
+  {
+    return this.http.put<any>(this.url + '/lights/' + lightId + '/state', {"on": false});
+  }
+
+  public turnOnLight(lightId:string):Observable<any>
+  {
+    return this.http.put<any>(this.url + '/lights/' + lightId + '/state', {"on": true});
+  }
+
+  public setLightState(lightId:string, state:LightStateInterface):Observable<any>
+  {
+    return this.http.put<any>(this.url + '/lights/' + lightId + '/state', state);
+  }
+
+  public getLightState(lightId:string):Observable<any>
+  {
+    return this.http.get<any>(this.url + '/lights/' + lightId);
+  }
+}
