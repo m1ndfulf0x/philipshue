56e22da47a3545ce4c0a71ed95acc2cf16f2a959
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2021-02-17 16:09:16 +0100

    install karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -9,13 +9,12 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-firefox-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
+        coverageReporter: {
             dir: require('path').join(__dirname, './coverage/philipshue'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true,
