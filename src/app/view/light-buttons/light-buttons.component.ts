00f688f0faa0b3e3fd550254bb4f8ba5fe6f124c
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2021-02-17 15:06:49 +0100

    run prettier

--- src/app/view/light-buttons/light-buttons.component.ts
+++ src/app/view/light-buttons/light-buttons.component.ts
@@ -20,12 +20,12 @@ export class LightButtonsComponent implements OnInit {
     constructor(private service: HueApiService, public stateService: LightStateService) {}
 
     public ngOnInit(): void {
-      this.textColor = '#000000';
+        this.textColor = '#000000';
         this.getTemperature();
     }
 
     public getTemperature(): void {
-      this.textColor = '#ffd740';
+        this.textColor = '#ffd740';
         this.service.getTemperature('43').subscribe((res) => {
             this.temperature = res.state.temperature;
             this.textColor = '#000000';
