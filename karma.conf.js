ddc6de3d20fd86eed9d57a06f70cfe4b071750b0
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2021-04-11 15:59:01 +0200

    change path of coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/philipshue'),
+            dir: 'coverage',
             reporters: [{type: 'html'}],
             check: {
               global: {
