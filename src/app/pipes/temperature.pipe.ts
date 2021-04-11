4692dca961eaa4771c60ac2df4eb88f32beb8bfd
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2021-04-11 16:07:13 +0200

    ng lint

--- src/app/pipes/temperature.pipe.ts
+++ src/app/pipes/temperature.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'temperaturePipe' })
 export class TemperaturePipe implements PipeTransform {
     transform(value: number): string {
-        let transformedValue: number = value / 100;
+        const transformedValue: number = value / 100;
         return transformedValue.toFixed(1).toString().replace('.', ',') + '°C';
     }
 }
