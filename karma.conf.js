5abcf645795b27c6fdea991a14ed9abaccb5c8df
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2022-09-09 11:48:21 +0200

    fix name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/copy-of-hue'),
+      dir: require('path').join(__dirname, './coverage/philipshue'),
       subdir: '.',
       reporters: [
         { type: 'html' },
